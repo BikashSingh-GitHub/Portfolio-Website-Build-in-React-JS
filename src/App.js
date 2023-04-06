8d983ef4776f61f06524ebddb0aa23915971ac93
Bikash Singh <blackhatsys@gmail.com>
2023-04-06 08:32:05 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import Navbar from "./Navbar";
+import Navbar3 from "./Navbar3";
+import Navbar4 from "./Navbar4";
+import ContactUs from "./ContactUs";
+import Comingsoon from "./Comingsoon"
+// import "./Navbar.css"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex flex-col h-screen">
+
+      <Navbar />
+      <Navbar3 />
+      <Navbar4 />
+{/* 
+<ContactUs/>
+<Comingsoon /> */}
+    
     </div>
   );
 }
